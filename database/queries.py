684909dde0764e54c61d6b618e61c909af3d2905
jaemukkim <kimjaemuk@gmail.com>
2026-03-22 15:37:45 +0900

    쿼리 추가

--- database/queries.py
+++ database/queries.py
@@ -1,10 +1,15 @@
-def get_all_capsules(conn): # 호출할 때 conn을 받음
+# 전체 목록 가져오기
+def get_all_capsules(conn):
     with conn.cursor() as cursor:
         cursor.execute("SELECT * FROM capsules")
         return cursor.fetchall()
 
 # 테이블 생성 함수
 def create_tables(conn):
+    if conn is None:
+        print("DB 연결이 유효하지 않습니다.")
+        return
+
     with conn.cursor() as cursor:
         # 캡슐 마스터 테이블
         cursor.execute("""
@@ -51,3 +56,20 @@ def consume_capsule(conn, capsule_id, drink_type="아이스 아메리카노"):
         print(f"Error: {e}")
         conn.rollback()
         return False
+
+# 캡슐 신규 등록 함수
+def add_capsule(conn, name, brand, intensity, acidity, stock_count):
+    """새로운 캡슐 정보를 추가합니다."""
+    try:
+        with conn.cursor() as cursor:
+            sql = """
+                INSERT INTO capsules (name, brand, intensity, acidity, stock_count)
+                VALUES (%s, %s, %s, %s, %s)
+            """
+            cursor.execute(sql, (name, brand, intensity, acidity, stock_count))
+        conn.commit()
+        return True
+    except Exception as e:
+        print(f"등록 에러: {e}")
+        conn.rollback()
+        return False        
